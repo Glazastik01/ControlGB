f7ef54decf311cc6458b49ec57c49aa1fc1df4a4
Kirill <vasolovk56@gmail.com>
2023-01-24 23:24:38 +0300

    добавлена программа

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] arr;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+arr = stroka.Split(' ');
+var result = new string[arr.Length];
+var realSize = 0;
+foreach (var value in arr)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result));
